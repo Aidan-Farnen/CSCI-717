efdaf3b38a87c4234755feec9f6f3979e58d9d61
Aidan Farnen <55867465+Aidan-Farnen@users.noreply.github.com>
2025-10-07 23:27:03 -0500

    Update EncryptionMachine.java
    implemented encryptWord() method. This method uses the encryptLetter() method to shift all the letters of a given word. This uses a string builder to append each letter to the new string after it has been shifted

--- Assignment 1/EncryptionMachine.java
+++ Assignment 1/EncryptionMachine.java
@@ -1,13 +1,12 @@
 package encryptionMachine;
 
 public class EncryptionMachine {
-	
+	// Class constants
 	public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     public static final int SHIFT = 3;
 
     public static void main(String[] args) {
-    	System.out.println("Encrypted 'a' → " + encryptLetter('a'));
-    	System.out.println("Encrypted 'y' → " + encryptLetter('y'));
+    	System.out.println("Encrypted 'play' → " + encryptWord("play"));
     }
 
     /**
@@ -19,6 +18,22 @@ public class EncryptionMachine {
         int index = ALPHABET.indexOf(letterInput);	//get index of character input to method
         int shiftedIndex = (index + SHIFT) % ALPHABET.length();	//get the index of letter after shifting
         return ALPHABET.charAt(shiftedIndex);	//return character after shifting
+    }
+    
+    /**
+     * Encrypts an entire word by encrypting each letter individually
+     * using the encryptLetter() method.
+     */
+    public static String encryptWord(String inputWord) {
+        StringBuilder outputWord = new StringBuilder();	//create new string builder
+
+        for (int i = 0; i < inputWord.length(); i++) {	//loop through each character in input word
+            char inputLetter = inputWord.charAt(i);	//get the character at the loop index
+            char shiftedLetter = encryptLetter(inputLetter);	//shift character with encryptLetter()
+            outputWord.append(shiftedLetter);	//append shifted character to string builder
+        }
+
+        return outputWord.toString();	//return new word made up of shifted characters
     }
 }
 
