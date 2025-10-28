e8ead300c1c31735d53be5c56230da18f0328ae3
Aidan Farnen <55867465+Aidan-Farnen@users.noreply.github.com>
2025-10-28 17:28:57 -0500

    cleaning up repo
    deleting files that were used to test other individual python files

--- Project/src/test_data.py
+++
@@ -1,6 +0,0 @@
-from data_loader import load_better_recipes, recipes_to_docs
-
-df = load_better_recipes("data/recipes.csv")
-print(df.head(2)[["recipe_name", "ingredients_str", "cuisine_path"]])
-docs = recipes_to_docs(df)
-print(docs[0].keys())
